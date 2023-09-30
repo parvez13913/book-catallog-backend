db0cc167142cdde5d8a9ee305825c30a88af9c7c
parvez13913 <parvezz13913@gmail.com>
2023-09-30 21:31:16 +0600

    [DONE] Category Validation

--- src/app/modules/category/category.route.ts
+++ src/app/modules/category/category.route.ts
@@ -1,13 +1,23 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { CategoryController } from './category.controller';
+import { CategoryValidations } from './category.validation';
 
 const router = express.Router();
 
-router.post('/create-category', CategoryController.createCategory);
+router.post(
+  '/create-category',
+  validateRequest(CategoryValidations.createCategoryZodSchema),
+  CategoryController.createCategory,
+);
 
 router.get('/:id', CategoryController.getSingleCategory);
 
-router.patch('/:id', CategoryController.updateCategory);
+router.patch(
+  '/:id',
+  validateRequest(CategoryValidations.updateCategoryZodSchema),
+  CategoryController.updateCategory,
+);
 
 router.get('/', CategoryController.getAllCategories);
 
---
+++ src/app/modules/category/category.validation.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+const createCategoryZodSchema = z.object({
+  body: z.object({
+    title: z.string({
+      required_error: 'Title is required',
+    }),
+  }),
+});
+
+const updateCategoryZodSchema = z.object({
+  body: z.object({
+    title: z.string({
+      required_error: 'Title is required',
+    }),
+  }),
+});
+
+export const CategoryValidations = {
+  createCategoryZodSchema,
+  updateCategoryZodSchema,
+};
