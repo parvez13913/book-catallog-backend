99cbcc542fb7c9ee63a558a063999751496350b0
parvez13913 <parvezz13913@gmail.com>
2023-09-30 21:10:22 +0600

    [DONE] uncaughtException and unhandledRejection

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express, { Application, NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import router from './app/routes';
 const app: Application = express();
 
@@ -11,6 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1', router);
+
+// GlobalErrorHandler
+app.use(globalErrorHandler);
 
 //handle not found
 app.use((req: Request, res: Response, next: NextFunction) => {
--- src/server.ts
+++ src/server.ts
@@ -6,13 +6,33 @@ import app from './app';
 import config from './config';
 
 async function bootstrap() {
-  try {
-    const server: Server = app.listen(config.port, () => {
-      console.log(`Server running on port ${config.port}`);
-    });
-  } catch (error) {
-    console.log('Fail to connect Database', error);
-  }
+  const server: Server = app.listen(config.port, () => {
+    console.log(`Server running on port ${config.port}`);
+  });
+
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        console.log('Server closed');
+      });
+    }
+    process.exit(1);
+  };
+
+  const unexpectedErrorHandler = (error: unknown) => {
+    console.log(error);
+    exitHandler();
+  };
+
+  process.on('uncaughtException', unexpectedErrorHandler);
+  process.on('unhandledRejection', unexpectedErrorHandler);
+
+  process.on('SIGTERM', () => {
+    console.log('SIGTERM received');
+    if (server) {
+      server.close();
+    }
+  });
 }
 
 bootstrap();
