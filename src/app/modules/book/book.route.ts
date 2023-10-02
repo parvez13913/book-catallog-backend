6bbc97f31228dc79e6bc23f9a2daeeef1ac2a509
parvez13913 <parvezz13913@gmail.com>
2023-10-02 14:47:26 +0600

    [DONE] Book validation

--- src/app/modules/book/book.route.ts
+++ src/app/modules/book/book.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { BookController } from './book.controller';
 import { BookValidations } from './book.validation';
@@ -7,6 +9,7 @@ const router = express.Router();
 
 router.post(
   '/create-book',
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(BookValidations.createBookZodSchema),
   BookController.createBook,
 );
@@ -17,11 +20,12 @@ router.get('/:id', BookController.getSingleBook);
 
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(BookValidations.updateBookZodSchema),
   BookController.updateBook,
 );
 
-router.delete('/:id', BookController.deleteBook);
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), BookController.deleteBook);
 
 router.get('/', BookController.getAllBooks);
 
