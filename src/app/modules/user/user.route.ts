3c9d8a0de82e60d523d4776ad11ac855f187642f
parvez13913 <parvezz13913@gmail.com>
2023-10-02 13:13:49 +0600

    [DONE] user validation

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,20 +1,23 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { UserController } from './user.controller';
 import { UserValidations } from './user.validation';
 
 const router = express.Router();
 
-router.get('/:id', UserController.getSingleUser);
+router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.getSingleUser);
 
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(UserValidations.updateUserZodSchema),
   UserController.updateUser,
 );
 
-router.delete('/:id', UserController.deleteUser);
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.deleteUser);
 
-router.get('/', UserController.getAllUsers);
+router.get('/', auth(ENUM_USER_ROLE.ADMIN), UserController.getAllUsers);
 
 export const UserRoutes = router;
